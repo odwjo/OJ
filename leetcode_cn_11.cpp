3ae82296b6e7431b63fed6f1ee4e874cbee5249b
Jhon <973127457@qq.com>
2020-02-05 08:49:55 +0800

    Create leetcode_cn_11.cpp
    双指针，移动小端

---
+++ leetcode_cn_11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ret = 0;
+        size_t i = 0, j = height.size() - 1;
+        
+        while (i != j) {
+            ret = max<int>(ret, (j - i) * min(height[i], height[j]));
+            if (height[i] < height[j]) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return ret;
+    }
+};
